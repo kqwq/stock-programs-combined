a37a7c3e48e6bc730a7d019640b82e81540f3f78
Kyle Wells <47221196+kqwq@users.noreply.github.com>
2023-06-10 22:09:49 -0400

    working

--- src/app/downloader/excel.ts
+++ src/app/downloader/excel.ts
@@ -13,112 +13,114 @@ async function fetchStock(ticker: string, date = null): Promise<any> {
   return json;
 }
 
-// function createStyles(workbook: WorkBook) {
-//   // Create a reusable style
-//   styleDefault = workbook.createStyle({
-//     // styleDefault
-//     font: {
-//       size: 12,
-//     },
-//   });
-//   styleHeader = workbook.createStyle({
-//     font: {
-//       size: 12,
-//     },
-//     border: {
-//       // §18.8.4 border (Border)
-//       bottom: {
-//         style: "thin",
-//         color: "black",
-//       },
-//     },
-//   });
+const styleDefault = {
+  // styleDefault
+  font: {
+    size: 12,
+  },
+  type: "g",
+};
+const styleHeader = {
+  font: {
+    size: 12,
+  },
+  border: {
+    // §18.8.4 border (Border)
+    bottom: {
+      style: "thin",
+      color: "black",
+    },
+  },
+  numberFormat: "n",
+};
 
-//   styleCurrency = workbook.createStyle({
-//     font: {
-//       size: 12,
-//     },
-//     numberFormat: "$#,##0.00;[Red]-$#,##0.00",
-//   });
-//   stylePercentage = workbook.createStyle({
-//     font: {
-//       color: "#000000",
-//       size: 12,
-//     },
-//     numberFormat: "#0.00%",
-//   });
-//   stylePercentageDown = workbook.createStyle({
-//     font: {
-//       color: "#FF0000",
-//       size: 12,
-//     },
-//     numberFormat: "#0.00%",
-//   });
-//   styleDate = workbook.createStyle({
-//     font: {
-//       color: "#000000",
-//       size: 12,
-//     },
-//     numberFormat: "yy-mmm-dd",
-//   });
-// }
+const styleCurrency = {
+  font: {
+    size: 12,
+  },
+  numberFormat: "$#,##0.00;[Red]-$#,##0.00",
+};
+const stylePercentage = {
+  font: {
+    color: "#000000",
+    size: 12,
+  },
+  numberFormat: "#0.00%",
+};
+const stylePercentageDown = {
+  font: {
+    color: "#FF0000",
+    size: 12,
+  },
+  numberFormat: "#0.00%",
+};
+const styleDate = {
+  font: {
+    color: "#00ff00",
+    size: 12,
+  },
+  type: "d",
+  numberFormat: "yy-mmm-dd",
+};
 
 const colData = [
   {
     name: "Volume",
     ref: "volume",
-    // style: styleDefault,
+    style: styleDefault,
     width: 1,
   },
   {
     name: "Call/Put",
     ref: () => callOrPut,
-    // style: styleDefault,
+    style: styleDefault,
     width: 1,
   },
   {
     name: "Symbol",
     ref: (o: any) => o.symbol,
-    // style: styleDefault,
+    style: styleDefault,
     width: 1,
   },
   {
     name: "Name",
     nameAbove: "Stock",
     ref: (o: any) => o.fullName,
-    // style: styleDefault,
+    style: styleDefault,
     width: 3,
   },
   {
     name: "Price",
     nameAbove: "Stock",
     ref: (o: any) => o.price,
-    // style: styleDefault,
+    style: styleDefault,
     width: 1,
   },
   {
     name: "Strike",
     ref: "strike",
-    // style: styleDefault,
+    style: styleDefault,
     width: 1,
   },
   {
     name: "Date",
     nameAbove: "Expiration",
-    ref: (o: any) => o.expiration / 86400 + 25569,
-    // style: styleDate,
+    ref: (o: any) => {
+      return parseInt(o.expiration) * 1000;
+    },
+    style: styleDate,
     width: 1.2,
   },
   {
     name: "Bid",
     ref: "bid",
-    // style: styleDefault,
+    style: styleDefault,
     width: 1,
   },
   {
     name: "Ask",
     ref: "ask",
-    // style: styleDefault,
+    style: styleDefault,
     width: 1,
   },
 ];
@@ -143,17 +145,25 @@ function generateSpreadsheet(
 
   // Set headers
   colData.forEach((col, colOffset) => {
-    callsSheet.cell(5, colOffset + 1).string(col.name);
-    // .style(styleHeader);
-    putsSheet.cell(5, colOffset + 1).string(col.name);
-    // .style(styleHeader);
-    callsSheet.column(colOffset + 1).setWidth(9 * col.width);
-    putsSheet.column(colOffset + 1).setWidth(9 * col.width);
+    XLSX.utils.sheet_add_aoa(callsSheet, [[col.name]], {
+      origin: { r: 4, c: colOffset },
+      cellStyles: true,
+    });
+    XLSX.utils.sheet_add_aoa(putsSheet, [[col.name]], {
+      origin: { r: 4, c: colOffset },
+      cellStyles: true,
+    });
+    callsSheet["!cols"] = callsSheet["!cols"] || [];
+    putsSheet["!cols"] = putsSheet["!cols"] || [];
+    callsSheet["!cols"][colOffset] = { width: 9 * col.width };
+    putsSheet["!cols"][colOffset] = { width: 9 * col.width };
     if (col.nameAbove) {
-      callsSheet.cell(4, colOffset + 1).string(col.nameAbove);
-      // .style(styleDefault);
-      putsSheet.cell(4, colOffset + 1).string(col.nameAbove);
-      // .style(styleDefault);
+      XLSX.utils.sheet_add_aoa(callsSheet, [[col.nameAbove]], {
+        origin: { r: 3, c: colOffset },
+      });
+      XLSX.utils.sheet_add_aoa(putsSheet, [[col.nameAbove]], {
+        origin: { r: 3, c: colOffset },
+      });
     }
   });
 
@@ -164,9 +174,27 @@ function generateSpreadsheet(
       colData.forEach((col, colOffset) => {
         let r = col.ref;
         let val = typeof r === "string" ? option[r] : r(company);
+        const numFormat =
+          "numberFormat" in col.style ? col.style.numberFormat : null;
         if (val !== undefined) {
-          callsSheet.cell(callRow + 5, colOffset + 1)[typeof val](val);
-          // .style(col.style); // Set symbol
+          XLSX.utils.sheet_add_aoa(
+            callsSheet,
+            [
+              [
+                {
+                  v: val,
+                  t: col.style.type,
+                  z: numFormat ?? undefined,
+                },
+              ],
+            ],
+            {
+              origin: {
+                r: callRow + 5,
+                c: colOffset,
+              },
+            }
+          );
         }
       });
       callRow++;
@@ -178,9 +206,24 @@ function generateSpreadsheet(
       colData.forEach((col, colOffset) => {
         let r = col.ref;
         let val = typeof r === "string" ? option[r] : r(company);
+        const numFormat =
+          "numberFormat" in col.style ? col.style.numberFormat : null;
         if (val !== undefined) {
-          putsSheet.cell(putRow + 5, colOffset + 1)[typeof val](val);
-          // .style(col.style); // Set symbol
+          XLSX.utils.sheet_add_aoa(
+            putsSheet,
+            [
+              [
+                {
+                  v: val,
+                  t: col.style.type,
+                  z: numFormat ?? undefined,
+                },
+              ],
+            ],
+            {
+              origin: { r: putRow + 5, c: colOffset },
+            }
+          );
         }
       });
       putRow++;
@@ -222,7 +265,9 @@ async function main(tickers: string[], updateFuncs: any) {
     let expDates = data.optionChain.result[0].expirationDates;
     for (let j = 0; j < expDates.length; j++) {
       let date = expDates[j];
-      const stockData = await fetchStock(ticker, date);
+      const res = await fetchStock(ticker, date);
+      const stockData = await res.json();
+
       let inner = stockData.optionChain.result[0];
       let options: any = inner.options[0];
       optionsCache.push({

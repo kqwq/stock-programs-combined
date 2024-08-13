3961ccba40e80f959869d87aa50c2e4053e76fe8
Kyle Wells <47221196+kqwq@users.noreply.github.com>
2024-08-13 15:07:47 -0400

    Update route.ts

--- src/app/api/stock/[ticker]/route.ts
+++ src/app/api/stock/[ticker]/route.ts
@@ -5,11 +5,16 @@ export async function GET(request: Request) {
   const ticker = url.pathname.split("/").at(-1);
   const date = url.searchParams.get("date");
 
-  const res = await fetch(
-    `https://query1.finance.yahoo.com/v7/finance/options/${ticker}${
-      date ? `?date=${date}` : ""
-    }`
-  );
+  // const res = await fetch(
+  //   `https://query1.finance.yahoo.com/v7/finance/options/${ticker}${
+  //     date ? `?date=${date}` : ""
+  //   }`
+  // );
+  const res = await fetch(`https://query1.finance.yahoo.com/v7/finance/options/${ticker}${ date ? `?date=${date}&` : "?"}crumb=rBcTyW4mPD.`, {
+    headers: {
+      cookie: 'A1=d=AQABBEllS2YCEORNKcURxAr9H9BGWa1EKfwFEgEBCAHrvGbwZtww0iMA_eMBAAcISWVLZq1EKfw&S=AQAAAqJquPgpFvSbVpvKaTCHBjo',
+    },
+  })
   const json = await res.json();
 
   return NextResponse.json(json, {
